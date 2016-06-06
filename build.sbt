7602be9ddec0f23a8083f371a337cabc6623ea84
aterenin <aterenin@users.noreply.github.com>
2016-06-06 08:55:41 -0700

    partially implemented comm & numeric code

--- build.sbt
+++ build.sbt
@@ -7,3 +7,10 @@ scalaVersion := "2.11.8"
 assemblyJarName in assembly := "AsyncGibbsMPI.jar"
 mainClass in assembly := Some("AsyncGibbsMPI")
 test in assembly := {}
+
+val breezeVersion = "0.12"
+
+libraryDependencies ++= Seq(
+  "org.scalanlp" %% "breeze" % breezeVersion,
+  "org.scalanlp" %% "breeze-natives" % breezeVersion
+)
---
+++ probit.R
@@ -0,0 +1,21 @@
+source("../TeX/AVTfunctions.R")
+
+generate.probit.data = function(n, beta, id=0, write=FALSE) { 
+  beta = matrix(beta, ncol=1)
+  p = nrow(beta)
+  x = matrix(rnorm(n*p),nrow=n,ncol=p)
+  y = pnorm(x %*% beta) %>% round
+  d = cbind(y,x) %>% data.frame %>% set_colnames(c("y", paste0("x",1:p))) 
+  if(write) {
+    write.table(d, paste0("data/probit-",id,".csv"), sep = ",", row.names = FALSE) 
+    write.table(t(x) %*% x, paste0("data/probit-xtx-",id,".csv"), sep = ",", row.names = FALSE) 
+  } else
+    return(d) 
+}
+
+beta = c(1.3,4,-1,1.6,5,-2,rep(0,394))
+
+generate.probit.data(1000,beta,1,TRUE)
+
+d = read.csv("data/probit-1.csv")
+xtx = read.csv("data/probit-xtx-1.csv")
